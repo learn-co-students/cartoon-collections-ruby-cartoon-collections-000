a35f6c326db52a684aaf70c1ceab6a01c13e15b7
Timothy Hart <timothyhart6@gmail.com>
2017-03-21 19:46:01 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,31 +1,24 @@
 def roll_call_dwarves(dwarves)
-dwarves.each_with_index do |d, i|
+  dwarves.each_with_index do |d, i|
    puts "#{i + 1}. #{d}"
  end
 end
 
 def summon_captain_planet(planeteer_calls)
-planeteer_calls.collect do |element|
-  element.capitalize << "!"
-end
+  planeteer_calls.collect do |element|
+    element.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls(words)
-words.any? do |word|
-  if word.length > 4
-    true
-  else
-    false
+  words.any? do |word|
+    word.length > 4
   end
- end
 end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.detect do |cheese|
-    if cheese.include?(cheese_types[0]) == true
-      cheese
-    else
-      end
-    end
+    cheese_types.include?(cheese)
+  end
 end
