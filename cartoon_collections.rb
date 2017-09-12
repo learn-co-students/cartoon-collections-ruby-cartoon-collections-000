897c7e45984371a4c969edfa80aee5dccf24a4fa
sharmina akhtar <sakht318@gmail.com>
2017-09-12 17:58:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,15 +9,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(short_words)
-  # Your code here
-  short_words.any? do |word|
-  word.length > 4
-  end
+  short_words.any? {|word| word.length > 4}
 end
 
 def find_the_cheese(cheese_type)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_type.find { |i| ["cheddar", "gouda", "camembert"].include? i }
-
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_type.find { |x| ["cheddar", "gouda", "camembert"].include?(x) }
 end
