7defb940e64b1ea479a20c70ba6dffd9e2aa53fc
Barney Fabian <bafa@tutanota.de>
2018-01-11 13:17:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-def roll_call_dwarves(dwarf_names)
-  dwarf_names.each.with_index(1) do |name, index| puts "#{index} #{name}" end
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |name, index| puts "#{index} #{name}" end
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |x| x.capitalize + "!" end
+def summon_captain_planet(array)
+  array.collect do |x| x.capitalize! + "!" end
 end
 
 def long_planeteer_calls(array)
