7ebdc2e8074a835879edb5714d2551b677dcb9da
grandballoon <luke.sovern.conley@gmail.com>
2018-06-12 15:44:36 -0400

    cartoon collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+      new_dwarves = []
+  array.each_with_index do |miner, index|
+    miner = "#{index + 1}. #{miner}"
+    new_dwarves << miner
+  end
+  puts new_dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call = "#{call.capitalize!}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(shouts)
+  shouts.any? do |shout|
+    shout.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(board)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  board.find do |item|
+    cheese_types.include? item
+  end
 end
