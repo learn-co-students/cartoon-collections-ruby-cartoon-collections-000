6b5e17e7a936fa7ad5eefd7e67a4e4a9f00b49a1
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-17 19:16:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each.with_index(0..3) do |dwarf|
+  end
+  puts roll_call_dwarves
+    # Your code here
 end
 
 def summon_captain_planet# code an argument here
