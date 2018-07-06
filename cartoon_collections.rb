234ed76f3cee53ffa96e0b58317e86287b6e3014
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-07-06 00:33:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,15 +4,18 @@ def roll_call_dwarves(dwarf_names)
   end
 end
 
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |planeteer_call|
     planeteer_call.capitalize << "!"
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(calls)
+  calls.include? call.length > 4
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
