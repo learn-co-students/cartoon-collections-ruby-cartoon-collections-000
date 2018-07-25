3431a20eec001d01229f5bfe25bbf7957ae80290
Stevenpaulino1 <stevenpaulino1@hotmail.com>
2018-07-25 15:36:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,15 @@
-def roll_call_dwarves(array)
-  array.each_with_index {|name, index| puts "#{index + 1} #{name}"}
+def roll_call_dwarves(dwarfNames)
+  dwarfNames.each_with_index {|name, index| puts "#{index + 1} #{name}"}
  end
  
- def summon_captain_planet(array)
-   array.collect do |call| 
+ def summon_captain_planet(planeteerCalls)
+   planeteerCalls.collect do |call| 
   call.capitalize + "!"
   end
  end 
 
-def long_planeteer_calls(array) 
-  if  array.any? {|i| i.length > 4}
+def long_planeteer_calls(calls) 
+  if  calls.any? {|i| i.length > 4}
     return true
   else 
     return false
@@ -17,14 +17,13 @@ def long_planeteer_calls(array)
 end
  #using any? by convention returns true without having to use return true/false. Also if I want to find out which words, then I would use .select 
 def find_the_cheese(array)
-   cheese_types = ["cheddar", "gouda", "camembert"] #types of cheeses
- array.each do |food| #iterating each element into food 
+   cheese_types = ["cheddar", "gouda", "camembert"] 
+ array.each do |food|  
     cheese_types.each do |cheese|
-      #turning each element into cheese 
       if food.include?(cheese)
         return food
       end
     end
-  end
-  return nil
+  end 
+    return nil 
  end
