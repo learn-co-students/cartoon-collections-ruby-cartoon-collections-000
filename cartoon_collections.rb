e46b10b3e5e5b4caf1fa3cc17343650971b16dbc
Nasser Sanou <nassersanou23@gmail.com>
2018-08-08 00:48:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,9 +34,22 @@ long_planeteer_calls(planeteer_calls)
 
 
 #Find the cheese 
-snacks_types =  ["tomato soup","oyster", "camembert", "cheddar", " crackers", "gouda"]
-def find_the_cheese(snacks_types)
-    snacks_types.find do |snacks| snacks_types.include?(cheese)
-    end
+# cheese_types = ["cheddar", "gouda", "camembert"]
+# def find_the_cheese(cheese_types)
+#     cheese_types.find do |cheese|
+#     if !cheese_types.include?(cheese)
+#       unless cheese.nil?
+#     else 
+#       puts "#{cheese}"
+#   end
+# end 
+# end
+
+def find_the_cheese(cheese)
+cheese_types = ["cheddar", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
-find_the_cheese(snacks_types)
+
+ find_the_cheese(["banana", "cheddar", "gouda", "camembert"])
