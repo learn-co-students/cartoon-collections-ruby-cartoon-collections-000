8fe62b7b879e8a17bb57c288e52dc555fedce739
Akua Apeagyei <apeagyei.akua@gmail.com>
2018-09-04 23:23:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,26 @@
 def roll_call_dwarves(names)
 names.each_with_index do |name, index|
-puts "#{index+1}.#{name}"
+puts "#{index + 1}.#{name}"
 end
 end
 
 def summon_captain_planet(planeteer_calls)
+planeteer_calls.collect {|call|
+call.capitalize << "!"}
 end
 
 def long_planeteer_calls(calls)
+if calls.length > 4
+return true
+else
+return false
+end
 end
 
 def find_the_cheese(strings)
 cheese_types = ["cheddar", "gouda", "camembert"]
+cheese_types.find do |cheese|
+strings.include?(cheese)
 end
+end
+
