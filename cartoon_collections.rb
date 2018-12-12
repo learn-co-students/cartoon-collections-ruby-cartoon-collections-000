a4d9cdfb78996910ffe3b47f8ca7f990ddb78672
Basiru Danso <basirudns@gmail.com>
2018-12-12 20:39:19 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,68 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  
+  names.each_with_index do |idx, ele|
+    
+    puts names
+    
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+puts roll_call_dwarves["Doc", "Dopey", "Bashful", "Grumpy"]
+
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(str)
+  names = str.split(" ")
+  new_name = []
+  
+  names.map! |name|
+    new_name << name[0].upcase + name[1..-1].downcase + "!"
+    
+  end
+  
+    return new_name
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+puts summon_captain_planet(planeteer_calls)
+
+
+short_words = ["puff", "go", "two"]
+
+def long_planeteer_calls(calls)
+  result = "" 
+  
+  i = 0 
+  calls.each_with_char do |call|
+    if call.length > 4
+      result = true 
+    else
+      result = false
+      
+      i += 1 
+    end
+    
+    return result
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+puts long_planteer_calls(short_words)
+
+
+
+def find_the_cheese(str)
+  snacks = ["cheddar", "gouda", "camembert"]
+  result = ""
+  
+  cheese_types.include? (snacks)
+      result << str
+    else
+      result = nil 
+    end
+      return result
 end
+
+puts find_the_cheese(snacks)
+
