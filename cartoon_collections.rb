914e66dc86c62dc140d53a06ee3745fe11d73742
funny-reference-4499 <davidbernier91@gmail.com>
2019-01-25 00:47:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  number = [1,2,3]
+
+	numbered_array = number.zip(dwarves).map { |pair| pair.insert(1, '. ').insert(3, ' ') }
+		roll_call = numbered_array.map {|phrase| phrase.join.chop}
+		return	roll_call.each { |dwarf|  puts dwarf }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map! { |word|  word.capitalize.concat('!') }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+	array.any? { |words|  words.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(possible_cheese)
+	  cheese_types = ["cheddar", "gouda", "camembert"]
+
+  	possible_cheese.detect { |type| cheese_types.include?(type) }
 end
