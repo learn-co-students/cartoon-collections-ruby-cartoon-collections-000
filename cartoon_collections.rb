01949ec5fe4de6349e2971b4dcb2294d6c7a988a
Colin Sidberry <colinsidberry@gmail.com>
2019-10-11 16:57:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,14 @@
+#1__________________________________________
 def roll_call_dwarves(array)
   array.each.with_index do |ele,i|
     puts (i+1).to_s + ". " + ele
   end
 end
-
+#2__________________________________________
 def summon_captain_planet(array)
   return array.collect{|ele| ele.capitalize + "!"}
 end
-
+#3__________________________________________
 def long_planeteer_calls(array) 
   array.each do |ele|
     if ele.length > 4
@@ -16,7 +17,7 @@ def long_planeteer_calls(array)
   end
   return false
 end
-
+#4__________________________________________
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |food|
