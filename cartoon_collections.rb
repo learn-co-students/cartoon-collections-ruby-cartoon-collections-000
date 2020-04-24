3bbbf327f0eb2ec1d65aa5624b9eea87dcf07c6c
Andrew C <acchang4@gmail.com>
2020-04-24 02:22:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,51 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.each_with_index do |dwarf, index|
+    indexplusone = index + 1
+    puts "#{indexplusone}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |x| x.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# I HAD TO TEST EACH METHOD AND I THOUGHT GOING THROUGH THEM SERIALLY
+# WOULD WORK BUT THEY DID NOT. IF I COULD NOT ATTACH, HOW CAN I USE?
+# # VIZ:   planeteer_calls.map do |x| x.capitalize
+#   end
+#   # planeteer_calls.map do |element| element << "!"
+#   end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def long_planeteer_calls(list)
+  list.collect do |x|
+    if x.size>4
+      return true
+    end
+      return false
+   end
+ end
+
+#looks like most people solved with .any? but this is not in the docs!
+#why is the end/return false needed, can we do without?
+#or an else in the loop? Also doesn't work for <4=false
+# Also why can't I use the below:
+#
+# def long_planeteer_calls(list)
+#   list.collect do |x|
+#     return true if x.size>4
+#     end
+#     return false
+#    end
+#  end
+
+def find_the_cheese(array)
+cheese_types = ["cheddar", "gouda", "camembert"]
+  array.collect do |x|
+  return x if cheese_types.include?(x)
+  end
+  nil
 end
