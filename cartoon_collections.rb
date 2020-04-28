74725f9e5b066d6f77db5c055348d483ed4755fe
Andrew C <acchang4@gmail.com>
2020-04-28 15:26:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,19 @@
 
+# #roll_call_dwarves
+#   prints out the 7 dwarfs in a numbered list
+
 def roll_call_dwarves(dwarf_array)
   dwarf_array.each_with_index do |dwarf, index|
     indexplusone = index + 1
     puts "#{indexplusone}. #{dwarf}"
   end
 end
+
+
+# #summon_captain_planet
+#   returns an array with the same number of elements that it was given
+#   capitalizes each element and adds an exclamation mark (test 1)
+#   capitalizes each element and adds an exclamation mark (test 2)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
@@ -13,10 +22,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-# I HAD TO TEST EACH METHOD AND I THOUGHT GOING THROUGH THEM SERIALLY
-# WOULD WORK BUT THEY DID NOT. IF I COULD NOT ATTACH, HOW CAN I USE?
-# # VIZ:   planeteer_calls.map do |x| x.capitalize
-#   end
+# Can I got through serially?
+#  VIZ:   planeteer_calls.map do |x| x.capitalize
 #   # planeteer_calls.map do |element| element << "!"
 #   end
 # answer: Yes, you could but it would slow down your program because you would loop twice
@@ -30,10 +37,16 @@ end
 #    end
 #  end
 
-#looks like most people solved with .any? but this is not in the docs!
-#why is the end/return false needed, can we do without?
-#or an else in the loop? Also doesn't work for <4=false
-#
+# def long_planteer_calls(planeteer_calls)
+#   planeteer_calls.any? do |call|
+#   call.length > 4
+#   end
+# end
+
+#long_planeteer_calls
+  #  returns true if any calls are longer than 4 characters
+  #  returns false if all calls are shorter than 4 characters
+
 def long_planeteer_calls(list)
   list.collect do |x|
     return true if x.size>4
@@ -41,8 +54,10 @@ def long_planeteer_calls(list)
     return false
    end
 
-#     return false if x.size<4 does not work because it asks for
-# if any of the calls are longer than 4, this asks if any less than 4.
+#find_the_cheese
+    # returns the first element of the array that is cheese
+    # returns nil if the array does not contain a type of cheese
+
 
 def find_the_cheese(array)
 cheese_types = ["cheddar", "gouda", "camembert"]
